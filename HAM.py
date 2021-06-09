22d5977f77a264bddf15eb8c5026ef57085fcc51
Keyi Liu <rayliu@Keyis-MacBook-Pro.local>
2021-06-09 15:22:38 -0400

    Update HAM.py

--- HAM.py
+++ HAM.py
@@ -6,7 +6,7 @@ from functools import reduce
 
 def readelectron(tag):
     raw = np.loadtxt('electrons.{}'.format(tag))[:, 4:]
-    return [[[ row[i*2 ] + 1j* row[i*2+1] for i in range(len(row)//2)] for row in raw ]]
+    return [[ row[i*2 ] + 1j* row[i*2+1] for i in range(len(row)//2)] for row in raw ]
 
 def readoffdiag():
     raw =  np.loadtxt('offdiag')
@@ -14,16 +14,18 @@ def readoffdiag():
     for i, row in enumerate(raw):
         c = 0
         for j in range(10):
-            for k in range(j, 10):
+            for k in range(j + 1, 10):
                 t[i][j][k] = t[i][k][j] = t[i][j+10][k+10] = t[i][k+10][j+10] = row[c]
                 c += 1
+    #print(t[1])
     return t
 
 
 def calpert(wfs, t):
-    return sum([reduce(np.multiply, np.conj(wf), t[i], wf) for i, wf in enumerate(wfs)])
+    return sum([reduce(np.dot, [np.conj(wf), t[i], wf]) for i, wf in enumerate(wfs)])
 
 if __name__ =='__main__':
+    t = readoffdiag()
     for tag in range(1, 13):
-        wfs, t = readelectron(tag), readoffdiag()
-        print(calpert(wfs, t))
+        wfs = readelectron(tag)
+        print('electron{}, shift:{}'.format(tag, np.real(calpert(wfs, t))))
